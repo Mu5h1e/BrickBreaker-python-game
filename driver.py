19872083e86323368777e3f5b99b4624bcdbe2d7
Mu5h1e <mushiesidd@gmail.com>
2021-02-10 12:33:40 +0530

    basic screen

--- driver.py
+++ driver.py
@@ -1,17 +1,27 @@
 import numpy as np
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
 
 class display_controller:
     def __init__(self):
-        self.width = 100
-        self.height = 100
+        self.width = 20
+        self.height = 20
         self.timer = 0
-        self.display_matrix = np.zeros((100,100))
+        self.display_matrix = np.zeros((self.width, self.height))
     
     def display_timer(self):
         return self.timer
     
-    def print_matrix(self):
-        print(self.display_matrix)
+    def init_matrix(self):
+        self.display_matrix[self.height-1][((self.width//2)-3):((self.width//2)+2)] = 1
+    
+    def refreshed_matrix(self):
+        for _ in range(self.height):
+            for __ in range(self.width):
+                print({True: " ", False: "1"}[self.display_matrix[_][__] == 0], end=" ")
+            print()
+
 
 class paddle_controller:
     def __init__ (self, position, input_key):
@@ -29,8 +39,9 @@ class paddle_controller:
 
 
 def driver_function():
-    dc1 = display_controller()
-    dc1.print_matrix()
+    dc = display_controller()
+    dc.init_matrix()
+    dc.refreshed_matrix()
 
 if __name__ == '__main__':
     driver_function()
